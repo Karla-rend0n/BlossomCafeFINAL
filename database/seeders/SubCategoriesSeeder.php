b4c70fe621fc39954e99c21c87dd1b97d6fafb67
Karla-rend0n <95234946+Karla-rend0n@users.noreply.github.com>
2022-11-21 00:47:10 -0600

    Update SubCategoriesSeeder.php

--- database/seeders/SubCategoriesSeeder.php
+++ database/seeders/SubCategoriesSeeder.php
@@ -17,32 +17,32 @@ class SubCategoriesSeeder extends Seeder
     {
         $subCategory1= new SubCategory;
         $subCategory1->nameSub = 'Bingsu';
-        $subCategory1->description='Se trata de hielo granizado con una especie de mermelada de judías rojas, pasta de arroz y polvo de frutos secos.';
-        $subCategory1->photo='Inserte photo';
+        $subCategory1->description='It is slush ice with a kind of red bean jam, rice paste and dried fruit powder.';
+        $subCategory1->image='subcategory/LmUvWe02hwkSPy0ORw4qXFuThQ19i2DRtKu8Bkis.jpg';
         $subCategory1->category_id =2;
 
         $subCategory1->save();
 
         $subCategory2= new SubCategory;
-        $subCategory2->nameSub = 'Cafe';
-        $subCategory2->description='El café es la bebida que se obtiene a partir de los granos tostados y molidos de los frutos de la planta del café; es altamente estimulante por su contenido de cafeína, una sustancia psicoactiva.';
-        $subCategory1->photo='Inserte photo';
+        $subCategory2->nameSub = 'Coffee';
+        $subCategory2->description='Coffee is the beverage obtained from the roasted and ground beans of the fruits of the coffee plant; it is highly stimulating due to its caffeine content, a psychoactive substance.';
+        $subCategory1->image='subcategory/mX4egiJTFlIHfZqHUMYAh2mvB4xpoFrCzS7cZSpO.jpg';
         $subCategory2->category_id =1;
 
         $subCategory2->save();
 
         $subCategory3= new SubCategory;
         $subCategory3->nameSub = 'Boba Tea';
-        $subCategory3->description='También conocido por su anglicismo bubble tea o también como boba, es una bebida de té dulce aromatizada inventada en Taiwán.';
-        $subCategory3->photo='Inserte photo';
+        $subCategory3->description='Also known by its anglicism bubble tea or also as boba, it is a sweet flavored tea drink invented in Taiwan.';
+        $subCategory3->image='subcategory/QWVOojLYtW5PRrUPPH1KdHeZs1PFSA9Ai2gMHdM8.jpg';
         $subCategory3->category_id =1;
 
         $subCategory3->save();
 
         $subCategory4= new SubCategory;
         $subCategory4->nameSub = 'Cake';
-        $subCategory4->description='';
-        $subCategory1->photo='Inserte photo';
+        $subCategory4->description=' a breadlike food made from a dough or batter that is usually fried or baked in small flat shapes and is often unleavened.';
+        $subCategory1->image='subcategory/gBxs2XM4Nm6jpwZTL46fNAib8MHXfr70Q916P71B.jpg';
         $subCategory4->category_id =2;
 
         $subCategory4->save();
