948a7ecd8ec168dc641962833f50f23fed11c5af
Karla-rend0n <95234946+Karla-rend0n@users.noreply.github.com>
2022-11-21 00:41:38 -0600

    Update SubCategoryController.php

--- app/Http/Controllers/Api/SubCategoryController.php
+++ app/Http/Controllers/Api/SubCategoryController.php
@@ -5,6 +5,13 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\SubCategory;
+use Illuminate\Support\File;
+
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Storage;
+use Carbon\Carbon;
+
+
 
 class SubCategoryController extends Controller
 {
@@ -18,12 +25,26 @@ class SubCategoryController extends Controller
     
     public function store(Request $request)
     {
-        $subCategory = new SubCategory();
-        $subCategory->nameSub=$request->nameSub;
-        $subCategory->description=$request->description;
-        $subCategory->photo=$request->photo;
-        $subCategory->category_id=$request->category_id;
-        $subCategory->save();
+        
+        try{
+            $subCategory= new SubCategory();
+            $subCategory ->nameSub = $request->input('nameSub');
+            $subCategory ->description = $request->input('description');
+            $subCategory ->image = $request->file('image')->store('subcategory');
+            $subCategory ->category_id = $request->input('category_id');
+            $subCategory->save();
+           
+
+            return response()->json([
+                'message'=>'SubCategory created successfully!'
+            ]);
+        }catch(\Exception $e){
+            \Log::error($e->getMessage());
+
+            return response()->json([
+                'message'=>'Something goes wrong while creating a subCategory!'
+            ], 500);
+        }
     }
 
     
@@ -34,16 +55,46 @@ class SubCategoryController extends Controller
     }
 
     
-    public function update(Request $request, $id)
-    {
-        $subCategory = SubCategory::findOrFail($request->id);
-        $subCategory->nameSub=$request->nameSub;
-        $subCategory->description=$request->description;
-        $subCategory->photo=$request->photo;
-        $subCategory->category_id=$request->category_id;
-        $subCategory->save();
-        
-        return $subCategory;
+    public function update(Request $request, SubCategory $subCategory)
+    {       
+         $request->validate([
+            'nameSub'=>'required',
+            'description'=>'required',
+            'category_id'=>'required',
+            'image'=>'required'
+
+         ]);
+
+         try{
+            $subCategory->fill($request->post())->update();
+
+            if($request->hasFile('image')) {
+                if($subCategory->image){
+                    $exists = Storage::disk('public')->exists("public/subcategory/{$subCategory->image}");
+
+                    if($exists){
+                        Storage::disk('public')->delete("public/subcategory/{$subCategory->image}");
+                    }
+
+                }
+
+                $imageName= Str::random() . '.' . $request->image->getClientOnExtension();
+                Storage::disk('public')->putFileAs('subcategory', $request->image, $imageName);
+                $subCategory->image = $imageName;
+                $subCategory->save();
+
+                return response()->json([
+                    'message'=>'SubCategory update successfully!'
+                ]);
+            }
+
+         } catch(\Exception $e){
+            \Log::error($e->getMessage());
+
+            return response()->json([
+                'message'=>'Something goes wrong while updating a subCategory!'
+            ], 500);
+        }
     }
 
     
